45e5394065955e1137a052b1e7372c7bf77d34b7
Jason Victor <jason_victor@outlook.com>
2024-12-29 14:38:14 -0500

    project files

---
+++ test.py
@@ -0,0 +1,7 @@
+from openai_module import generate_text_basic
+
+prompt = "Explain the concept of AI agents in one sentence."
+
+response = generate_text_basic(prompt, model="gpt-4")
+
+print(response)
